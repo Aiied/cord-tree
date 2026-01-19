105ac11fc560e9e0e61407af9ba59e62780ff6c3
Aiied <90443112+Aiied@users.noreply.github.com>
2026-01-19 15:04:43 +0900

    Title: 황금비율 토스트, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260119/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+int n, m;
+string s;
+
+int main() {
+    cin >> n >> m;
+    cin >> s;
+    string st;
+    cin >> st;
+    list<char>l;
+    list<char>::iterator it;
+    
+    it = l.end();
+
+    for (int i = 0; i < m; i++) {
+        char command;
+        cin >> command;
+        if (command == 'P') {
+            char c;
+            cin >> c;
+            l.insert(it,c);
+        }
+        else if(command == 'L'){
+            if(it!=l.begin()){
+                it--;
+            }
+        }
+        else if(command == 'R'){
+            if(it!=l.end()){
+                it++;
+            }
+        }
+        else{
+            if(it!=l.end()){
+                l.erase(it);
+            }                        
+        }
+
+
+    }
+    for(it = l.begin(); it!=l.end(); it++){
+        cout << *it;
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
